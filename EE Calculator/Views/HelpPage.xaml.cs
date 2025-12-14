16b47570bef04444c6efee3d5425ba5424449a04
Idan Regev <iregev@microsoft.com>
2025-12-14 15:31:47 +0200

    Blank help page

---
+++ EE Calculator/Views/HelpPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using EE_Calculator.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace EE_Calculator.Views
+{
+    public sealed partial class HelpPage : Page
+    {
+        public HelpViewModel ViewModel { get; } = new HelpViewModel();
+
+        public HelpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
